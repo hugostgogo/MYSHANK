198fc9a475258913df2428abc102ef6c6521778e
Hugo <huglemoal@gmail.com>
2021-02-25 13:17:09 +0100

    Clean

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          0
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          1
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          2
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
