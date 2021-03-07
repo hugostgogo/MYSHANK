09ebff6dd062ac5090a7d9d84c9f28bb7b8f7670
Hugo <huglemoal@gmail.com>
2021-03-07 18:09:55 +0100

    Source selection ok

--- src/background.js
+++ src/background.js
@@ -93,20 +93,12 @@ if (isDevelopment) {
 // CUSTOM
 
 
-ipcMain.handle('setSource', (event, arg) => {
+ipcMain.handle('setSource', (event, pin) => {
   rpio.open(16, rpio.OUTPUT, rpio.LOW);
+  rpio.open(18, rpio.OUTPUT, rpio.LOW);
+  rpio.open(22, rpio.OUTPUT, rpio.LOW);
 
-  rpio.open(15, rpio.INPUT)
+  rpio.write(pin, rpio.HIGH);
 
-  for (var i = 0; i < 5; i++) {
-          /* On for 1 second */
-          rpio.write(16, rpio.HIGH);
-          rpio.sleep(1);
-          console.log(rpio.read(16));
-          /* Off for half a second (500ms) */
-          rpio.write(16, rpio.LOW);
-          rpio.msleep(500);
-          console.log(rpio.read(16));
-  }
-  return 'Pin 15 is currently ' + rpio.read(16) + rpio.read(15)
+  return { phono: rpio.read(16), lineIn: rpio.read(18), feedBack: rpio.read(22), }
 })
