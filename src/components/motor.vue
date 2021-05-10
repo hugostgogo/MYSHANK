b93500bff5ace4a5f90c23913f825f0861b19af4
Hugo <huglemoal@gmail.com>
2021-05-11 00:25:20 +0200

    delays saves

--- src/components/motor.vue
+++ src/components/motor.vue
@@ -2,11 +2,11 @@
 <v-card class="mt-2 pa-3 px-5 d-flex align-center" style="flex-grow: 1">
     <v-flex>
       <v-flex class="d-flex justify-space-between align-center">
-        <span class="text-h5">Motor speed <span v-if="status.speed">: {{ speed }} %</span></span>
-        <v-switch v-model="status.speed" @change="setSpeed"></v-switch>
+        <span class="text-h5">Motor speed <span v-if="speedSat">: {{ speed }} %</span></span>
+        <v-switch v-model="speedSat" @change="setSpeed"></v-switch>
       </v-flex>
       <v-fade-transition>
-        <v-progress-linear v-if="status.speed" top v-model="speed" height="50"></v-progress-linear>
+        <v-progress-linear v-if="speedSat" top v-model="speed" height="50"></v-progress-linear>
       </v-fade-transition>
     </v-flex>
 </v-card>
@@ -24,6 +24,9 @@ import { mapGetters, mapMutations } from 'vuex'
         'speed',
         'speedStatus',
       ]),
+      speedSat () {
+        return this.speedStatus
+      }
     },
     methods: {
       ...mapMutations([
