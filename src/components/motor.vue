3b6aa784d39d00a58f9e82eb04e2971fd247ddab
Hugo <huglemoal@gmail.com>
2021-05-16 18:53:48 +0200

    motor & heating

--- src/components/motor.vue
+++ src/components/motor.vue
@@ -2,7 +2,7 @@
 <v-card class="mt-2 pa-3 px-5 d-flex align-center" style="flex-grow: 1">
     <v-flex>
       <v-flex class="d-flex justify-space-between align-center">
-        <span class="text-h5">Motor speed <span v-if="speedSatus">: {{ speed }} %</span></span>
+        <span class="text-h5">Motor speed <span v-if="speedStatus">: {{ speed }} %</span></span>
         <v-switch v-model="speedSat" @change="setSpeed"></v-switch>
       </v-flex>
       <v-fade-transition>
