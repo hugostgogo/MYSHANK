4d424a712a65503a991d1f0694fbd723e819b455
Hugo <huglemoal@gmail.com>
2021-05-21 14:58:43 +0200

    functions design fix

--- src/components/functions.vue
+++ src/components/functions.vue
@@ -1,14 +1,15 @@
 <template>
 <v-flex class="d-flex" style="height: 100%; width: 100%" xs12>
-      <v-card @click="leadIn" class="mr-3 pa-3 col-3 d-flex flex-column justify-center align-center" style="flex-grow: 1" v-ripple="{ class: 'primary--text' }">
-          <v-icon size="100">mdi-contain-start</v-icon>
-          <v-card-title>LEAD IN</v-card-title>
-      </v-card>
+    <v-card @click="leadIn" class="mr-3 pa-3 d-flex flex-column justify-center align-center" style="flex-grow: 1" v-ripple="{ class: 'primary--text' }">
+        <v-icon size="100">mdi-contain-start</v-icon>
+        <v-card-title>LEAD IN</v-card-title>
+    </v-card>
 
-      <v-card @click="space" class="pa-3 col-3 d-flex flex-column justify-center align-center" style="flex-grow: 1" v-ripple="{ class: 'primary--text' }">
-        <v-icon size="100">mdi-keyboard-space</v-icon>
-        <v-card-title>SPACE</v-card-title>
-      </v-card>
+
+    <v-card @click="space" class="pa-3 d-flex flex-column justify-center align-center" style="flex-grow: 1" v-ripple="{ class: 'primary--text' }">
+      <v-icon size="100">mdi-keyboard-space</v-icon>
+      <v-card-title>SPACE</v-card-title>
+    </v-card>
 </v-flex>
 </template>
 <script>
@@ -19,6 +20,6 @@ export default {
       'space',
       'leadIn'
     ])
-  }
+  },
 }
 </script>
